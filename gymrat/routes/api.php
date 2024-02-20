31b31b2ecf0bfef9bb4fb90b485cbd8bcc87a06b
Angelica F. Padilla <152839578+Angelicapadilla13@users.noreply.github.com>
2024-02-20 18:46:49 +0800

    DB,API

--- gymrat/routes/api.php
+++ gymrat/routes/api.php
@@ -22,9 +22,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::apiResource('user', UserController::class);
 Route::apiResource('program', ProgramController::class);
